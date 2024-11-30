684ac3a21fb03f018cb9dcfd19700082f72c5c39
dev-optic <prathamjain2011@gmail.com>
2024-11-30 11:21:32 +0530

    first commit

---
+++ variables.js
@@ -0,0 +1,43 @@
+var xyz = 'Pratham'; //old method
+let abc = 'Pratham'; //new method
+
+//or, we can do this
+
+let firstName;
+firstName = 'Pratham'
+
+//we can also assign two or more variables on the same line
+let color = 'blue', lastName = 'Jain';
+
+//ways to initialise string data
+let favoriteFruit = 'pineapple'
+let favoriteIceCream = "black current"
+let favoriteProgrammingLanguage = `Flutter`
+
+//number data
+let donuts = 12;
+let pi = 3.141526;
+
+//bigint data or big integers
+let veryLargeNumber = 21411412412n;
+
+//boolean data - true or false
+let lovesCoding = true;
+
+//undefined data 
+let favoriteColor;
+
+//null data
+favoriteFruit = null;
+
+//symbol data
+const uniqueKey = Symbol();
+
+//object - can store a lot of variables in one - can be int or string
+let course = {
+    name: 'JavaScript for Beginners',
+    hours: 3
+};
+console.log(course);
+
+//from 2024 Pratham Jain 8A 21
